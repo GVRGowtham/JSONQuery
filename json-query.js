747d8fb8325d4bbb8d234be898f892a13f2f340c
Gowtham Venkat <GVRGowtham@users.noreply.github.com>
2019-01-17 16:54:27 +0530

    Changes for ES6

--- json-query.js
+++ json-query.js
@@ -1,5 +1,3 @@
-"use strict";
-exports.__esModule = true;
 /*
 
 An ORM for JSON datatables
@@ -9,187 +7,187 @@ Created By Venkat
 Supports all basic CRUD operations
 
 */
-var JSONQuery = /** @class */ (function () {
-    function JSONQuery(jsonData) {
+module.exports = class JSONQuery {
+    constructor(jsonData) {
         this.json = [];
         this.json = jsonData;
     }
     //Insert a new row in the table
-    JSONQuery.prototype.insert = function (data) {
+    insert(data) {
         this.json.push(data);
-    };
+    }
     //Update a particular column with a new value based on a condition
     /*Currently supports greater than, lesser than, equal to, not equal to,
     lesser than or equal to,greter than or equal to,
     like and ilike operators*/
-    JSONQuery.prototype.update = function (field, condition, value, updateField, data) {
+    update(field, condition, value, updateField, data) {
         if (condition === "eq") {
-            for (var i = 0; i < this.json.length; i++) {
+            for (let i = 0; i < this.json.length; i++) {
                 if (this.json[i][field] === value) {
                     this.json[i][updateField] = data;
                 }
             }
         }
         else if (condition === "neq") {
-            for (var i = 0; i < this.json.length; i++) {
+            for (let i = 0; i < this.json.length; i++) {
                 if (this.json[i][field] !== value) {
                     this.json[i][updateField] = data;
                 }
             }
         }
         else if (condition === "gt") {
-            for (var i = 0; i < this.json.length; i++) {
+            for (let i = 0; i < this.json.length; i++) {
                 if (this.json[i][field] > value) {
                     this.json[i][updateField] = data;
                 }
             }
         }
         else if (condition === "lt") {
-            for (var i = 0; i < this.json.length; i++) {
+            for (let i = 0; i < this.json.length; i++) {
                 if (this.json[i][field] < value) {
                     this.json[i][updateField] = data;
                 }
             }
         }
         else if (condition === "eqgt") {
-            for (var i = 0; i < this.json.length; i++) {
+            for (let i = 0; i < this.json.length; i++) {
                 if (this.json[i][field] >= value) {
                     this.json[i][updateField] = data;
                 }
             }
         }
         else if (condition === "eqlt") {
-            for (var i = 0; i < this.json.length; i++) {
+            for (let i = 0; i < this.json.length; i++) {
                 if (this.json[i][field] <= value) {
                     this.json[i][updateField] = data;
                 }
             }
         }
         else if (condition === "like") {
-            for (var i = 0; i < this.json.length; i++) {
+            for (let i = 0; i < this.json.length; i++) {
                 if (this.json[i][field].indexOf(value) > -1) {
                     this.json[i][updateField] = data;
                 }
             }
         }
         else if (condition === "ilike") {
-            for (var i = 0; i < this.json.length; i++) {
+            for (let i = 0; i < this.json.length; i++) {
                 if (this.json[i][field].toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1) {
                     this.json[i][updateField] = data;
                 }
             }
         }
-    };
+    }
     //Replace the entire data based on a condition
-    JSONQuery.prototype.replace = function (field, value, data) {
-        for (var i = 0; i < this.json.length; i++) {
+    replace(field, value, data) {
+        for (let i = 0; i < this.json.length; i++) {
             if (this.json[i][field] === value) {
                 this.json[i] = data;
             }
         }
-    };
+    }
     //Delete a row based on a condition
-    JSONQuery.prototype.remove = function (field, value) {
-        for (var i = 0; i < this.json.length; i++) {
+    remove(field, value) {
+        for (let i = 0; i < this.json.length; i++) {
             if (this.json[i][field] === value) {
                 this.json.splice(i, 1);
             }
         }
-    };
+    }
     //Select only few columns to be shown
-    JSONQuery.prototype.select = function (fields) {
-        var selectedJson = [];
+    select(fields) {
+        let selectedJson = [];
         if (fields.length === 0) {
-            for (var i = 0; i < this.json.length; i++) {
-                var data = this.json[i];
+            for (let i = 0; i < this.json.length; i++) {
+                let data = this.json[i];
                 selectedJson.push(data);
             }
         }
         else {
-            for (var i = 0; i < this.json.length; i++) {
-                var data = {};
-                for (var j = 0; j < fields.length; j++) {
+            for (let i = 0; i < this.json.length; i++) {
+                let data = {};
+                for (let j = 0; j < fields.length; j++) {
                     data[fields[j]] = this.json[i][fields[j]];
                 }
                 selectedJson.push(data);
             }
         }
         return new JSONQuery(selectedJson);
-    };
+    }
     //Filter the data based on a query
     /*Currently supports greater than, lesser than, equal to, not equal to,
     lesser than or equal to, greter than or equal to,
     like and ilike operators*/
-    JSONQuery.prototype.filter = function (field, condition, value) {
+    filter(field, condition, value) {
         if (condition === "eq") {
-            var filteredJson = this.json.filter(function (entry) {
+            let filteredJson = this.json.filter(entry => {
                 return entry[field] === value;
             });
             return new JSONQuery(filteredJson);
         }
         else if (condition === "neq") {
-            var filteredJson = this.json.filter(function (entry) {
+            let filteredJson = this.json.filter(entry => {
                 return entry[field] !== value;
             });
             return new JSONQuery(filteredJson);
         }
         else if (condition === "gt") {
-            var filteredJson = this.json.filter(function (entry) {
+            let filteredJson = this.json.filter(entry => {
                 return entry[field] > value;
             });
             return new JSONQuery(filteredJson);
         }
         else if (condition === "lt") {
-            var filteredJson = this.json.filter(function (entry) {
+            let filteredJson = this.json.filter(entry => {
                 return entry[field] < value;
             });
             return new JSONQuery(filteredJson);
         }
         else if (condition === "eqgt") {
-            var filteredJson = this.json.filter(function (entry) {
+            let filteredJson = this.json.filter(entry => {
                 return entry[field] >= value;
             });
             return new JSONQuery(filteredJson);
         }
         else if (condition === "eqlt") {
-            var filteredJson = this.json.filter(function (entry) {
+            let filteredJson = this.json.filter(entry => {
                 return entry[field] <= value;
             });
             return new JSONQuery(filteredJson);
         }
         else if (condition === "like") {
-            var filteredJson = this.json.filter(function (entry) {
+            let filteredJson = this.json.filter(entry => {
                 return entry[field].indexOf(value) > -1;
             });
             return new JSONQuery(filteredJson);
         }
         else if (condition === "ilike") {
-            var filteredJson = this.json.filter(function (entry) {
+            let filteredJson = this.json.filter(entry => {
                 return entry[field].toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1;
             });
             return new JSONQuery(filteredJson);
         }
-    };
+    }
     //Sort the output ascending or descending based on a column
     //Currently supports string, number, float data types only
-    JSONQuery.prototype.sort = function (field, ascending) {
-        var tempjson = JSON.parse(JSON.stringify(this.json));
+    sort(field, ascending) {
+        let tempjson = JSON.parse(JSON.stringify(this.json));
         if (ascending) {
-            return new JSONQuery(tempjson.sort(function (a, b) {
+            return new JSONQuery(tempjson.sort((a, b) => {
                 return ((a[field] < b[field]) ? -1 : ((a[field] > b[field]) ? 1 : 0));
             }));
         }
         else {
-            return new JSONQuery(tempjson.sort(function (a, b) {
+            return new JSONQuery(tempjson.sort((a, b) => {
                 return ((b[field] < a[field]) ? -1 : ((b[field] > a[field]) ? 1 : 0));
             }));
         }
-    };
-    JSONQuery.prototype.union = function (result) {
-        var new_json = result.getResults();
+    }
+    union(result) {
+        const new_json = result.getResults();
         while (new_json.length > 0) {
-            var insert_data = new_json.pop();
-            var i = void 0;
+            const insert_data = new_json.pop();
+            let i;
             for (i = 0; i < this.json.length; i++) {
                 if (this.json[i] === insert_data) {
                     break;
@@ -200,11 +198,9 @@ var JSONQuery = /** @class */ (function () {
             }
         }
         return new JSONQuery(this.json);
-    };
+    }
     //Get the results as JSON array
-    JSONQuery.prototype.getResults = function () {
+    getResults() {
         return this.json;
-    };
-    return JSONQuery;
-}());
-exports.JSONQuery = JSONQuery;
+    }
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+var jsonquery = require('./json-query');
+
+let xyz = new jsonquery([]);
+
+//Now we can use xyz as a json query object
